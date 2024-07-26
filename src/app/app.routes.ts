793976c520b2f5b8b11998561425398b4d805c28
KasunChethiya <kcrathnamalala@gmail.com>
2024-07-26 16:06:00 +0530

    add routes to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { AddCustomerComponent } from './page/add-customer/add-customer.component';
+import { AddHardwareItemComponent } from './page/add-hardware-item/add-hardware-item.component';
+import { RentalFormComponent } from './page/rental-form/rental-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"add-customer",
+        component:AddCustomerComponent
+    }
+    {
+        path:"add-hardware-item",
+        component:AddHardwareItemComponent
+    }
+    {
+        path:"rental-form",
+        component:RentalFormComponent
+    }
+];
